0bce00d5f9e92e512bef21f6f95af3eb028ffa51
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2026-05-14 15:58:27 +0300

    add payment class and readme for future docs

--- payment-service/payment_service.py
+++ payment-service/payment_service.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Payment(BaseModel):
+    user_id: int
+    payment_id: int
+    amount: float
+    item: str
+
 @app.get('/')
 def default_get():
-    ...
+    print('get')
 
 @app.post('/')
-def default_post():
-    ...
+def default_post(payment: Payment):
+    print(f'{payment=}')
