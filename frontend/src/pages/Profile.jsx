20647d949411e1a01ac53cb70052c2a318df4df4
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2026-05-17 21:54:51 +0300

    add basic design of profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom"
+import "./Profile.css"
+import "../style.css"
+
 function Profile() {
-    return <div></div>;
+    return <div className="profile-main">
+        <div className="buttons">
+            <Link to="/register">
+                <button className="profile-btn typ-button">Register</button>
+            </Link>
+            <Link>
+                <button className="profile-btn typ-button">Login</button>
+            </Link>
+        </div>
+    </div>;
 }
 
 export default Profile;
